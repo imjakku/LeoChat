5038bdf89c59d5d525423d96bc9e8847409fea4d
imjakku <jatin.sharma02@motherson.com>
2024-12-26 16:06:36 +0530

    V3

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule), provideHttpClient()]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,38 +1,47 @@
-import { CommonModule, JsonPipe, NgClass } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../chat.service';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, NgClass, CommonModule],
+  imports: [FormsModule, NgClass, CommonModule, NgxSpinnerModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   userInput: string = '';
   context: string = "";
-
   messages: { sender: string, text: any }[] = [];
-  botRes: any;
   secondAttribute: any = "";
-  constructor(private chatService: ChatService) { }
+  data: string = "";
+  isLoading: boolean = false;
+
+  constructor(private chatService: ChatService, private spinner: NgxSpinnerService) { }
 
   sendMessage() {
     if (this.userInput.trim()) {
-      this.messages.push({ sender: 'user', text: this.userInput });
-      this.chatService.getChatResponse(this.userInput, this.context).subscribe(response => {
+      this.userInput = this.userInput.replace(/[\r\n]+$/, '').trim();
+      this.data = this.userInput;
+      this.messages.push({ sender: 'user', text: this.data });
+      this.userInput = '';
+      this.isLoading = true;
+      this.spinner.show();
+      this.chatService.getChatResponse(this.data, this.context).subscribe(response => {
         const responseKeys = Object.keys(response);
         this.secondAttribute = response[responseKeys[1]];
         console.log("Second attribute: ", this.secondAttribute);
         this.messages.push({ sender: 'bot', text: this.secondAttribute });
-      });
+        this.isLoading = false;
+        this.spinner.hide();
+      },
+        err => {
+          this.isLoading = false;
+          this.spinner.hide();
+        });
       console.log(this.messages);
-      this.userInput = '';
     }
   }
-
-
-
 }
